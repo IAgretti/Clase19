9f8400dcf16ce139e336e329ac2fd095238428c6
Iri <89229099+IAgretti@users.noreply.github.com>
2023-10-06 09:01:55 -0300

    Uso argumentos de la línea de comandos

---
+++ Clase19/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Clase19;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            // Console.WriteLine("Los argumentos que ha ingresado son: ");
+            // foreach (var arg in args)
+            // {
+            //     Console.WriteLine("* - " + arg);
+            // }
+            
+             Console.WriteLine($"Contador de parámetros = {args.Length}");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Arg[{i}] = [{args[i]}]");
+            }
+    }
+}
